213d328414a0e2e8a0fcfb3987cf4f83f4b66919
David Wright <david.wright@smoothstack.com>
2021-11-13 15:13:52 -0600

    Add SummaryMoMData tuple

--- Week 1 Assesment/my_types.py
+++ Week 1 Assesment/my_types.py
@@ -4,4 +4,13 @@ from collections import namedtuple
 RollingMoMData = namedtuple(
     "RollingMoMData", ["promoters", "passives", "dectractors"])
 
+SummaryMoMData = namedtuple(
+    "SummaryMoMData",
+    ["calls_offered",
+     "abandoned_after_30s",
+     "fcr",
+     "dsat",
+     "csat"]
+)
+
 MonthYear = namedtuple("MonthYear", ["month", "year"])
