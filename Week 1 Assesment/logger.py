4275ca4f78d3b5a7dd9bbb6c1c5614369469f9e1
David Wright <david.wright@smoothstack.com>
2021-11-13 16:26:46 -0600

    Add date to logs

--- Week 1 Assesment/logger.py
+++ Week 1 Assesment/logger.py
@@ -1,13 +1,15 @@
+from datetime import datetime
 from my_types import RollingMoMData, SummaryMoMData
 
 
 def log_message(message):
-    print(message)
+    formatted_time = datetime.now().strftime("%b-%d-%X")
+    print(f"[{formatted_time}] {message}")
 
 
 def log_summary_rolling_MoM(data: SummaryMoMData):
-    print(
-        f"Calls Offered {data.calls_offered}\n"
+    log_message(
+        f"\nCalls Offered {data.calls_offered}\n"
         f"Abandoned After 30s {data.abandoned_after_30s}\n"
         f"FCR {data.fcr}\n"
         f"DSAT {data.dsat}\n"
@@ -20,8 +22,8 @@ def log_VOC_rolling_MoM(data: RollingMoMData):
     passive_score = "good" if data.passives > 100 else "bad"
     decractor_score = "good" if data.dectractors > 100 else "bad"
 
-    print(
-        f"Promoters {promoter_score}\n"
+    log_message(
+        f"\nPromoters {promoter_score}\n"
         f"Passives {passive_score}\n"
         f"Decractors {decractor_score}\n"
     )
