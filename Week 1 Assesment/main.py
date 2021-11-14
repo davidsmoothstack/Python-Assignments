4c988a3e8a12241c211899c91477ca1eb872507a
David Wright <david.wright@smoothstack.com>
2021-11-14 13:41:21 -0600

    Change file_path to excel_path

--- Week 1 Assesment/main.py
+++ Week 1 Assesment/main.py
@@ -7,11 +7,11 @@ import util
 from my_types import SummaryData, VOCData
 
 
-def get_sheet(file_path, sheet_name):
-    """Returns an excel sheet in the form of a list of DataFrames from the specified file"""
-    logging.debug(f"Loading up {file_path} with sheet name {sheet_name}")
+def get_sheet(excel_path, sheet_name):
+    """Returns an excel sheet in the form of a list of DataFrames from the specified excel path"""
+    logging.debug(f"Loading up {excel_path} with sheet name {sheet_name}")
 
-    with pd.ExcelFile(file_path) as xlsx:
+    with pd.ExcelFile(excel_path) as xlsx:
         if sheet_name not in xlsx.sheet_names:
             logging.error(f"Failed to load sheet: {sheet_name}")
             exit(1)
@@ -19,18 +19,18 @@ def get_sheet(file_path, sheet_name):
         return pd.read_excel(xlsx, sheet_name)
 
 
-def get_summary_data(file_path):
+def get_summary_data(excel_path):
     """Returns a SummaryData named tuple that contains data from the "Summary Rolling MoM" sheet
 
     This function determines the column to read from based on the month and year located in the file name
     """
-    logging.debug(f"Parsing summary from {file_path}")
+    logging.debug(f"Parsing summary from {excel_path}")
 
-    excel = get_sheet(file_path, "Summary Rolling MoM")
+    excel = get_sheet(excel_path, "Summary Rolling MoM")
 
     # sheet.keys()[0] Gets a reference to the first unnamed column
     date_col = excel[excel.keys()[0]]
-    file_date = util.get_file_datetime(file_path)
+    file_date = util.get_file_datetime(excel_path)
 
     for row_index, row_date in enumerate(date_col):
         if (row_date.month, row_date.year) == (file_date.month, file_date.year):
@@ -41,17 +41,17 @@ def get_summary_data(file_path):
     exit(1)
 
 
-def get_VOC_data(file_path):
+def get_VOC_data(excel_path):
     """Returns a VOCData named tuple that contains data from the "VOC Rolling MoM" sheet
 
     This function determines the column to read from based on the month and year located in the file name
     """
-    logging.debug(f"Parsing VOC from {file_path}")
+    logging.debug(f"Parsing VOC from {excel_path}")
 
-    fileMonth, fileYear = util.get_file_monthyear(file_path)
+    fileMonth, fileYear = util.get_file_monthyear(excel_path)
     col_date = util.get_datetime(fileMonth, fileYear)
 
-    excel = get_sheet(file_path, "VOC Rolling MoM")
+    excel = get_sheet(excel_path, "VOC Rolling MoM")
 
     # Use the month string from file if the date column does not exist
     target_col = excel[col_date] if col_date in excel else excel[fileMonth.title()]
@@ -65,10 +65,10 @@ def get_VOC_data(file_path):
 
 if __name__ == "__main__":
     try:
-        sheet_path = util.get_console_input()
+        excel_path = util.get_console_input()
 
-        summary_data = get_summary_data(sheet_path)
-        voc_data = get_VOC_data(sheet_path)
+        summary_data = get_summary_data(excel_path)
+        voc_data = get_VOC_data(excel_path)
 
         logger.log_summary_data(summary_data)
         logger.log_VOC_data(voc_data)
