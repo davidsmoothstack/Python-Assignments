388f30fc30e653905180b657cddf8e343d9d03e7
David Wright <david.wright@smoothstack.com>
2021-11-21 02:38:27 -0600

    Validation

--- Week 2 Assignment/main.py
+++ Week 2 Assignment/main.py
@@ -1,8 +1,9 @@
 import logging
 import os
+import re
 import sys
 from pathlib import Path
-from typing import List
+from numpy import clongdouble
 
 import pandas as pd
 from pandas.core.frame import DataFrame
@@ -47,33 +48,79 @@ def write_to_file(file_path, content):
         file.write(content + "\n")
 
 
-def validate_phonenumber(df: DataFrame):
-    pattern = "\(?\(d{3})\)?(-|\s+)\1\2\d{4}"
+def is_processed_file(file_path):
+    with open(file_path) as file:
+        return file_path in file.readlines()
 
 
-def validate_email(df: DataFrame):
-    pattern = ".+@.+"
+def validate_phonenumbers(df: DataFrame, col_name):
+    pattern = r"\d{3}\.\d{3}\.\d{4}"
+    regex = re.compile(pattern)
+
+    for i, number in enumerate(df[col_name]):
+        if not regex.match(number):
+            logging.warn(f"Invalid number {number} at row {i}")
 
 
-def process_file(file_path):
-    pass
+def validate_emails(df: DataFrame, col_name):
+    pattern = r".+@.+"
+    regex = re.compile(pattern)
 
-# def replace_headers(csv_path):
-#     replacements = {
-#         "Agent Writing Contract Start Date (Carrier appointment start date)": "Agent Writing Contract Start Date",
-#         "Agent Writing Contract Status (actually active and cancelled\'s should come in two different files)": "Agent Writing Contract Status"
+    for i, email in enumerate(df[col_name]):
+        if not regex.match(email):
+            logging.warn(f"Invalid email {email} at row {i}")
+
+
+def validate_states(df: DataFrame, col_name):
+    # TODO: List all valid states
+    valid = []
+
+    for i, state in enumerate(df[col_name]):
+        if state not in valid:
+            logging.warn(f"Invalid state {state} found at row {i}")
+
+
+def replace_headers(df: DataFrame):
+    replacements = [
+        ("Agent Writing Contract Start Date (Carrier appointment start date)",
+         "Agent Writing Contract Start Date"),
+        ("Agent Writing Contract Status (actually active and cancelled\'s should come in two different files)",
+            "Agent Writing Contract Status")
+    ]
+
+    for (before, after) in replacements:
+        if before in df.columns:
+            df[before].rename(after)
+
+    return df
+
+
+def process_file(df: DataFrame):
+    df = replace_headers(df)
+
+    validate_phonenumbers(df, "Agency Phone Number")
+    validate_states(df, "Agency State")
+    validate_emails(df, "Agent Email Address")
+
+    return df
 
 
 if __name__ == "__main__":
     try:
-        files = get_ordered_files("Week 2 Assignment/in")
+        files = get_ordered_files("in")
         variance = line_diff(files[-1], files[-2])
 
         if variance > 500:
-            logging.error("Line variance is too long. Could not process file")
-            exit(1)
+            raise "Line variance is too long. Could not process file"
+
+        if is_processed_file(files[-1]):
+            raise "File is already processed"
 
         write_to_file("NYL.lst", files[-1].as_posix())
 
+        file_df = pd.read_csv(files[-1])
+        processed = process_file(file_df)
+
+        print(processed)
     except:
         logging.exception("")
